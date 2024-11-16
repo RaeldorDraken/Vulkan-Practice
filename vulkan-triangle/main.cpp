4b98dfb01cd85883d66ad202ce1f0c6dc7a98b37
Eduard Ros giralt <eros-gir@car11s1.42barcelona.com>
2024-11-16 19:15:25 +0100

    Finished the open window code

--- vulkan-triangle/main.cpp
+++ vulkan-triangle/main.cpp
@@ -6,7 +6,71 @@
 /*   By: eros-gir <eros-gir@student.42barcelona.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/17 17:14:21 by eros-gir          #+#    #+#             */
-/*   Updated: 2024/11/16 17:38:08 by eros-gir         ###   ########.fr       */
+/*   Updated: 2024/11/16 19:14:40 by eros-gir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#define GLFW_INCLUDE_VULKAN
+#include<GLFW/glfw3.h>
+
+#include<iostream>
+#include<stdexcept>
+#include<cstdlib>
+
+const uint32_t	WIDTH	= 800;
+const uint32_t	HEIGHT	= 600;
+
+class	HelloTriangleApplication
+{
+	public:
+		void	run()
+		{
+			initWindow();
+			initVulkan();
+			mainLoop();
+			cleanup();
+		}
+	private:
+		GLFWwindow*	window;
+
+		void	initWindow()
+		{
+			glfwInit();
+
+			glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+			glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+			window = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+		}
+		void	initVulkan()
+		{}
+		void	mainLoop()
+		{
+			while (!glfwWindowShouldClose(window))
+			{
+				glfwPollEvents();
+			}
+		}
+		void	cleanup()
+		{
+			glfwDestroyWindow(window);
+			glfwTerminate();
+		}
+};
+
+int	main()
+{
+	HelloTriangleApplication	app;
+
+	try
+	{
+		app.run();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return	EXIT_FAILURE;
+	}
+
+	return	EXIT_SUCCESS;	
+}
